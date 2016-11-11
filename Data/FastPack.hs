bae6c44e700174ad1f3e060c1d23da1d93fae478
Erik de Castro Lopo <erikd@mega-nerd.com>
2016-11-12 09:57:07 +1100

    FastPack proof of concept
    Can pack Word8, Word16, Word32, Word64 and ByteString literals
    and variables. For Word16/Word32/Word64 these must be specified
    to be either big or little endian (same approach as Binary).
    Example:
    
      putTest :: Word16 -> Word32 -> ByteString -> ByteString
      putTest x y bs =
        $(runFastPack
            [ PackBsVar "bs"        -- The variable named "bs"
            , PackNumVar "x" (PackW16 LE)
            , PackNumLit 0x3838 (PackW16 LE)
            , PackNumVar "y" (PackW32 BE)
            , PackBsLit " whatever "
            ])

--- Data/FastPack.hs
+++ Data/FastPack.hs
@@ -1,2 +1,139 @@
-module Data.FastPack where
+{-# LANGUAGE CPP #-}
 
+module Data.FastPack
+    ( Endian (..)
+    , PackData (..)
+    , PackNumType (..)
+    , runFastPack
+    ) where
+
+import           Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as BS
+import           Data.Either (partitionEithers)
+import qualified Data.List as DL
+import           Language.Haskell.TH.Syntax
+
+
+data PackData
+    = PackNumVar String PackNumType
+    | PackNumLit Integer PackNumType
+    | PackBsLit ByteString
+    | PackBsVar String
+    deriving (Eq, Show)
+
+data PackNumType
+    = PackW8
+    | PackW16 Endian
+    | PackW32 Endian
+    | PackW64 Endian
+    deriving (Eq, Show)
+
+data Endian = BE | LE deriving (Eq, Show)
+
+
+
+
+
+-- What do with ByteString parameters?
+-- Maybe also return a list of `String -> Int` functions?
+packDataSize :: [PackData] -> Exp
+packDataSize pds =
+    let (ls, rs) = partitionEithers $ map packSize pds in
+    mkSizeExp (DL.foldl' (+) 0 ls) rs
+  where
+    packSize pd =
+        case pd of
+            PackNumVar _ t -> Left $ sizePackNumType t
+            PackNumLit _ t -> Left $ sizePackNumType t
+            PackBsLit bs -> Left $ BS.length bs
+            PackBsVar s -> Right $ AppE (mkVarE "Data.FastPack.Functions.bsLength") (mkVarE s)
+    sizePackNumType t =
+        case t of
+            PackW8 {}-> 1
+            PackW16 {} -> 2
+            PackW32 {} -> 4
+            PackW64 {} -> 8
+
+    mkSizeExp :: Int -> [Exp] -> Exp
+    mkSizeExp known [] = mkLitE $ fromIntegral known
+    mkSizeExp known xs =
+        InfixE (Just . mkLitE $ fromIntegral known) numPlus $ sumExp xs
+
+    sumExp [] = Nothing
+    sumExp [x] = Just x
+    sumExp (x:xs) = Just $ InfixE (Just x) numPlus (sumExp xs)
+
+    numPlus = mkVarE "Data.FastPack.Functions.numPlus"
+
+
+runFastPack :: [PackData] -> Q Exp
+runFastPack xs = do
+    pure $ LetE [ValD (VarP $ mkName "size") (NormalB calcSize) []]
+            (AppE (AppE (mkVarE "Data.FastPack.Functions.bsUnsafeCreate") (mkVarE "size")) (builder 0 xs))
+  where
+    calcSize = packDataSize xs
+
+
+builder :: Int -> [PackData] -> Exp
+builder _ [] = LamE [WildP] (AppE (mkVarE "Data.FastPack.Functions.pure") (mkVarE "Data.FastPack.Functions.unit"))
+builder n (x:xs) =
+    let ptr = mkName $ "ptr" ++ show n in
+    LamE [VarP ptr]
+        (InfixE (Just (pokePackData ptr x))
+                (mkVarE "Data.FastPack.Functions.>>=") (Just (builder (n + 1) xs)))
+
+pokePackData :: Name -> PackData -> Exp
+pokePackData ptr pd =
+    AppE (AppE poker (VarE ptr)) (valueToExp pd)
+  where
+    poker =
+        case pd of
+            PackNumVar _ t -> pokeType t
+            PackNumLit _ t -> pokeType t
+            PackBsLit _ -> mkVarE "Data.FastPack.Functions.pokeAdvanceBS"
+            PackBsVar _ -> mkVarE "Data.FastPack.Functions.pokeAdvanceBS"
+    pokeType t =
+        case t of
+            PackW8 -> mkVarE "Data.FastPack.Functions.pokeAdvanceW8"
+            PackW16 {} -> mkVarE "Data.FastPack.Functions.pokeAdvanceW16"
+            PackW32 {} -> mkVarE "Data.FastPack.Functions.pokeAdvanceW32"
+            PackW64 {} -> mkVarE "Data.FastPack.Functions.pokeAdvanceW64"
+
+
+valueToExp :: PackData -> Exp
+valueToExp pd =
+    case pd of
+        PackNumVar s t -> wrapEndian t $ mkVarE s
+        PackNumLit i t -> wrapEndian t $ mkLitE i
+        PackBsLit bs -> LitE (StringL $ BS.unpack bs)
+        PackBsVar s -> mkVarE s
+  where
+    wrapEndian t ex =
+        case t of
+            PackW8 -> ex
+            PackW16 end -> endian end ex "Data.FastPack.Functions.bswapW16"
+            PackW32 end -> endian end ex "Data.FastPack.Functions.bswapW32"
+            PackW64 end -> endian end ex "Data.FastPack.Functions.bswapW64"
+
+    endian end ex n
+        | end == systemEndianness = ex
+        | otherwise = AppE (mkVarE n) ex
+
+
+
+
+
+
+mkVarE :: String -> Exp
+mkVarE = VarE . mkName
+
+mkLitE :: Integer -> Exp
+mkLitE = LitE . IntegerL
+
+
+systemEndianness :: Endian
+#ifdef WORDS_BIGENDIAN
+systemEndianness = BE
+#else
+systemEndianness = LE
+#endif
---
+++ Data/FastPack/Functions.hs
@@ -0,0 +1,91 @@
+{-# LANGUAGE MagicHash #-}
+module Data.FastPack.Functions
+    ( module GB
+    , bsLength
+    , bsUnsafeCreate
+    , bswapW16
+    , bswapW32
+    , bswapW64
+    , numPlus
+    , pokeAdvanceBS
+    , pokeAdvanceW8
+    , pokeAdvanceW16
+    , pokeAdvanceW32
+    , pokeAdvanceW64
+    , unit
+    ) where
+
+
+import           Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Internal as BSI
+
+import           Foreign.Storable (Storable (..))
+import           Foreign.ForeignPtr (withForeignPtr)
+import           Foreign.Ptr (Ptr, castPtr, plusPtr)
+
+import qualified GHC.Base as GB
+import           GHC.Word (Word8, Word16 (..), Word32 (..), Word64 (..))
+
+
+{-# INLINE bsLength #-}
+bsLength :: ByteString -> Int
+bsLength = BS.length
+
+{-# INLINE bsUnsafeCreate #-}
+bsUnsafeCreate :: Int -> (Ptr Word8 -> IO ()) -> ByteString
+bsUnsafeCreate = BSI.unsafeCreate
+
+{-# INLINE bswapW16 #-}
+bswapW16 :: Word16 -> Word16
+bswapW16 (W16# w#) = W16# (GB.narrow16Word# (GB.byteSwap16# w#))
+
+{-# INLINE bswapW32 #-}
+bswapW32 :: Word32 -> Word32
+bswapW32 (W32# w#) = W32# (GB.narrow32Word# (GB.byteSwap32# w#))
+
+{-# INLINE bswapW64 #-}
+bswapW64 :: Word64 -> Word64
+bswapW64 (W64# w#) = W64# (GB.byteSwap64# w#)
+
+{-# INLINE numPlus #-}
+numPlus :: Num a => a -> a -> a
+numPlus = (+)
+
+{-# INLINE pokeAdvanceBS #-}
+pokeAdvanceBS :: Ptr Word8 -> ByteString -> IO (Ptr Word8)
+pokeAdvanceBS destptr (BSI.PS fp offset len) =
+    withForeignPtr fp $ \ srcptr -> do
+        BSI.memcpy destptr (plusPtr srcptr offset) len
+        pure (plusPtr destptr len)
+
+{-# INLINE pokeAdvanceW8 #-}
+pokeAdvanceW8 :: Ptr Word8 -> Word8 -> IO (Ptr Word8)
+pokeAdvanceW8 = pokeAdvance
+
+{-# INLINE pokeAdvanceW16 #-}
+pokeAdvanceW16 :: Ptr Word8 -> Word16 -> IO (Ptr Word8)
+pokeAdvanceW16 = pokeAdvance
+
+{-# INLINE pokeAdvanceW32 #-}
+pokeAdvanceW32 :: Ptr Word8 -> Word32 -> IO (Ptr Word8)
+pokeAdvanceW32 = pokeAdvance
+
+{-# INLINE pokeAdvanceW64 #-}
+pokeAdvanceW64 :: Ptr Word8 -> Word64 -> IO (Ptr Word8)
+pokeAdvanceW64 = pokeAdvance
+
+{-# INLINE unit #-}
+unit :: ()
+unit = ()
+
+-- -----------------------------------------------------------------------------
+-- We don't export this one, because we want the TH splice to only use the
+-- explicitly typed versions so we can get type errors when the types are
+-- wrong.
+{-# INLINE pokeAdvance #-}
+pokeAdvance :: Storable a => Ptr Word8 -> a -> IO (Ptr Word8)
+pokeAdvance ptr a = do
+    poke (castPtr ptr) a
+    pure (plusPtr ptr (sizeOf a))
+

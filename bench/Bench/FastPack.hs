485a4e29398f4c0651742f3546e543436f7b9fa6
Erik de Castro Lopo <erikd@mega-nerd.com>
2016-11-13 20:50:14 +1100

    Update benchmark
    FastPack benchmark code now uses the proper TH version of the
    idea. The TH generated implementation is just as fast as the
    initial handrolled version it replaces. It should actually be
    producing exactly the same code.

--- bench/Bench/FastPack.hs
+++ bench/Bench/FastPack.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE Strict #-}
+{-# LANGUAGE TemplateHaskell #-}
 {-# OPTIONS_GHC -funbox-strict-fields #-}
 module Bench.FastPack where
 
@@ -6,32 +7,18 @@ module Bench.FastPack where
 import           Bench.Types
 
 import           Data.ByteString (ByteString)
-import qualified Data.ByteString.Internal as BSI
-import           Data.Word (Word8)
 
-import           Foreign.Storable (Storable, poke, sizeOf)
-import           Foreign.Ptr (Ptr, castPtr, plusPtr)
-
-
--- This version is just for benchmarking the idea.
--- The code being benchmarked here is what FastPack should generate from an
--- EDSL/TemplateHaskell/whatever.
-
-
-{-# INLINE pokeAdvance #-}
-pokeAdvance :: Storable a => Ptr Word8 -> a -> IO (Ptr Word8)
-pokeAdvance ptr a = do
-    poke (castPtr ptr) a
-    pure (plusPtr ptr (sizeOf a))
+import           Data.FastPack
+import qualified Data.FastPack.Functions
 
 
 {-# NOINLINE putBenchWord #-}
 putBenchWord :: BenchWord -> ByteString
 putBenchWord (BenchWord w8 w16 w32 w64) =
-    BSI.unsafeCreate (sizeOf w8 + sizeOf w16 + sizeOf w32 + sizeOf w64)
-    (       \ p0 -> pokeAdvance p0 w8
-        >>= \ p1 -> pokeAdvance p1 w16
-        >>= \ p2 -> pokeAdvance p2 w32
-        >>= \ p3 -> pokeAdvance p3 w64
-        >>= \ _ -> pure ()
-        )
+    $(runFastPack
+        [ PackNumVar "w8" PackW8
+        , PackNumVar "w16" (PackW16 LE)
+        , PackNumVar "w32" (PackW32 LE)
+        , PackNumVar "w64" (PackW64 LE)
+        ])
+

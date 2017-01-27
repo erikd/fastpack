4f3f920393a15d563c267e6008d26f83e3e272f0
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2017-01-27 13:40:11 +1100

    Use testbench for running tests + benchmarks
    This doesn't have quite the same behaviour as before; specifically,
    the sanityCheck checked a specific value whereas the new behaviour
    ensures that all libraries produce the same result, no matter what
    that result may be.
    
    Signed-off-by: Erik de Castro Lopo <erikd@mega-nerd.com>

--- bench/bench.hs
+++ bench/bench.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DataKinds, KindSignatures, RankNTypes, TypeApplications #-}
 
 import qualified Bench.Binary as Binary
 import qualified Bench.Cereal as Cereal
@@ -9,57 +10,66 @@ import           Bench.Types
 import           Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.List as DL
+import           Data.Proxy (Proxy(..))
 
-import qualified Criterion.Main as C
-
+import TestBench
 
 main :: IO ()
-main = do
-    sanityCheck
-    putStrLn "\nPassed sanity test.\n"
-    C.defaultMain benchmarks
-
+main =
+  testBench $ do
+    compareFunc "Write to ByteString"
+                (`withLibrary` (\l -> putBenchTest (putBenchWord l) bws))
+                (uncurry baseline (head libraries))
+                (mapM_ (uncurry comp) (tail libraries))
+    compareFunc "Read from ByteString"
+               (`withLibrary` (\l -> getBenchTest (getBenchWord l) bss))
+               (uncurry baseline (head libraries))
+               (mapM_ (uncurry comp) (tail libraries))
+  where
+    (bws, bss) = genBenchData 100000
 
 --------------------------------------------------------------------------------
 -- The benchmarks.
 
-benchmarks :: [C.Benchmark]
-benchmarks =
-    let (bws, bss) = genBenchData 100000 in
-    [ C.bgroup "Write to ByteString"
-        [ C.bench "Binary"      $ C.whnf (putBenchTest Binary.putBenchWord) bws
-        , C.bench "Cereal"      $ C.whnf (putBenchTest Cereal.putBenchWord) bws
-        , C.bench "Packer"      $ C.whnf (putBenchTest Packer.putBenchWord) bws
-        , C.bench "FastPack"    $ C.whnf (putBenchTest FastPack.putBenchWord) bws
-        ]
-    , C.bgroup "Read from ByteString"
-        [ C.bench "Binary"      $ C.whnf (getBenchTest Binary.getBenchWord) bss
-        , C.bench "Cereal"      $ C.whnf (getBenchTest Cereal.getBenchWord) bss
-        , C.bench "Packer"      $ C.whnf (getBenchTest Packer.getBenchWord) bss
-        , C.bench "FastPack"    $ C.whnf (getBenchTest FastPack.getBenchWord) bss
-        ]
-    ]
+data Library = Binary
+             | Cereal
+             | Packer
+             | FastPack
+  deriving (Eq, Ord, Show, Read, Enum, Bounded)
 
+libraries :: [(String, Library)]
+libraries = map ((,) =<< show) [minBound .. maxBound]
 
+withLibrary :: Library -> (forall l. (PutGet l) => Proxy l -> k) -> k
+withLibrary Binary   k = (k (Proxy @'Binary))
+withLibrary Cereal   k = (k (Proxy @'Cereal))
+withLibrary Packer   k = (k (Proxy @'Packer))
+withLibrary FastPack k = (k (Proxy @'FastPack))
+
+class PutGet (l :: Library) where
+
+  putBenchWord :: Proxy l -> BenchWord -> ByteString
+
+  getBenchWord :: Proxy l -> ByteString -> BenchWord
+
+instance PutGet 'Binary where
+  putBenchWord _ = Binary.putBenchWord
+  getBenchWord _ = Binary.getBenchWord
+
+instance PutGet 'Cereal where
+  putBenchWord _ = Cereal.putBenchWord
+  getBenchWord _ = Cereal.getBenchWord
+
+instance PutGet 'Packer where
+  putBenchWord _ = Packer.putBenchWord
+  getBenchWord _ = Packer.getBenchWord
+
+instance PutGet 'FastPack where
+  putBenchWord _ = FastPack.putBenchWord
+  getBenchWord _ = FastPack.getBenchWord
 
 putBenchTest :: (BenchWord -> ByteString) -> [BenchWord] -> Int
 putBenchTest put = DL.foldl' (\ acc bw -> acc + BS.length (put bw)) 0
 
 getBenchTest :: (ByteString -> BenchWord) -> [ByteString] -> Int
 getBenchTest get = DL.foldl' (\ acc bs -> acc + fromIntegral (getThird $ get bs)) 0
-
-
-sanityCheck :: IO ()
-sanityCheck = do
-    assert "Binary" $ Binary.sanityBenchWord bw == bw
-    assert "Cereal" $ Cereal.sanityBenchWord bw == bw
-    assert "Packer" $ Packer.sanityBenchWord bw == bw
-    assert "FastPack" $ FastPack.sanityBenchWord bw == bw
-  where
-    bw = BenchWord  0x123456789abcdef  0xabcdef123456789
-                    0x87654321 0x76543210 0x1234 0x2345 0x55 0xaa
-
-    assert name prop =
-        if prop
-            then pure ()
-            else error $ name ++ " failed assertion!"
